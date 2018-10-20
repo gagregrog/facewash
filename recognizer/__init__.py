d299c69f759f56a9668b5044e28982c4e3fe8597
Robert Reed <robert.mc.reed@gmail.com>
2018-10-20 11:03:03 -0700

    added extract and train convenience to recognizer

--- recognizer/__init__.py
+++ recognizer/__init__.py
@@ -3,6 +3,7 @@ import pickle
 import cv2
 import os
 from recognizer.extractor import Extractor
+from recognizer.trainer import train_model
 
 minDim = 10
 dirname = os.path.dirname(__file__)
@@ -12,17 +13,34 @@ default_le_path = os.path.sep.join([dirname, 'data', 'pickle', 'le.pickle'])
 
 
 class Recognizer:
-    def __init__(self, recognizer=default_recognizer_path, le=default_le_path, width=600, min_conf=0.5, min_dim=minDim):
-        with open(recognizer, 'rb') as f:
-            self.recognizer = pickle.loads(f.read())
-
-        with open(le, 'rb') as f:
-            self.le = pickle.loads(f.read())
+    def __init__(self, recognizer_path=default_recognizer_path, le_path=default_le_path, width=600, min_conf=0.5, min_dim=minDim, embedding_path=None):
+        self.recognizer_path = recognizer_path
+        self.le_path = le_path
+        self.recognizer = None
+        self.le = None
+        self.embedding_path = embedding_path
+        self.load_models()
 
         self.extractor = Extractor(width=width, min_conf=min_conf, min_dim=minDim)
         self.width = width
 
+    def load_models(self):
+        try:
+            with open(self.recognizer_path, 'rb') as f:
+                self.recognizer = pickle.loads(f.read())
+
+            with open(self.le_path, 'rb') as f:
+                self.le = pickle.loads(f.read())
+
+        except Exception as e:
+            print('Failed to read recognizer pickles.')
+            print(e)
+
     def recognize(self, image, draw=False, with_prob=False, colors=None):
+        if self.le is None:
+            print('Recognizer models not loaded.')
+            return
+
         w = image.shape[1]
         r = w / self.width
 
@@ -56,3 +74,20 @@ class Recognizer:
 
     def recognize_and_draw(self, image, colors=None):
         self.recognize(image, draw=True, with_prob=True, colors=colors)
+
+    def extract_and_train(self, training_images_path=None):
+        extractor_args = {}
+        trainer_args = {}
+
+        if self.embedding_path is not None:
+            extractor_args['output'] = self.embedding_path
+            trainer_args['embedding_path'] = self.embedding_path
+
+        if training_images_path is not None:
+            extractor_args['training_images_path'] = training_images_path
+
+        self.extractor.extract_and_write_embeddings(**extractor_args)
+
+        train_model(**trainer_args, recognizer_path=self.recognizer_path, le_path=self.le_path)
+
+        self.load_models()
